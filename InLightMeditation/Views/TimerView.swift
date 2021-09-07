64e6d64b5999767525af54003dd334a9a32507ae
konradgnat <konradmgnat@gmail.com>
2021-09-06 20:19:00 -0500

    timer

--- InLightMeditation/Views/TimerView.swift
+++ InLightMeditation/Views/TimerView.swift
@@ -13,15 +13,23 @@ struct TimerView: View {
     @State var isSoundOn: Bool = true
     @State var isRunning: Bool = false
     
+    @State var hours: Int = 0
+    @State var minutes: Int = 0
+    @State var seconds: Int = 0
+    
+    @State var timerIsPaused: Bool = true
+    @State var timer: Timer? = nil
+    
     var body: some View {
         ZStack(alignment: .center) {
             VStack(alignment: .leading) {
                 Spacer()
                 HStack {
                     Spacer()
-                    Text("\(self.time):00")
+                    Text(renderTime())
                         .font(Font.system(size: 38, design: .default))
                         .frame(width: 140, height: 100, alignment: .center)
+                        .padding(.all, 10)
                         .onTapGesture {
                             self.editTime = true
                         }
@@ -31,7 +39,7 @@ struct TimerView: View {
                 HStack {
                     Spacer()
                     Button(action: {
-                        print("tapped")
+                        self.startTimer()
                     }) {
                         HStack {
                             Text("start")
@@ -58,6 +66,51 @@ struct TimerView: View {
                 self.time = text
             })
         }
+    }
+    func renderTime() -> String {
+        if self.timerIsPaused {
+            return "\(self.time):00"
+        } else {
+            return "\(self.minutes < 10 ? "0\(self.minutes)" : String(self.minutes)):\(self.seconds < 10 ? "0\(self.seconds)" : String(self.seconds))"
+        }
+    }
+    func startTimer() {
+        if !self.timerIsPaused {
+            self.stopTimer()
+            return
+        }
+        self.resetTimer()
+        self.timerIsPaused = false
+        self.minutes = Int(self.time)!
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { tempTimer in
+            if seconds == 0 && self.minutes == 0 {
+                print("timer complted!")
+                self.stopTimer()
+                self.resetTimer()
+            }
+            if self.seconds == 0 {
+                self.seconds = 59
+                if self.minutes == 0 {
+                    self.minutes = 59
+                    self.hours -= 1
+                } else {
+                    self.minutes -= 1
+                }
+            } else {
+                self.seconds -= 1
+            }
+            
+        }
+    }
+    func stopTimer() {
+        self.timerIsPaused = true
+        self.timer?.invalidate()
+        self.timer = nil
+    }
+    func resetTimer() {
+        self.hours = 0
+        self.minutes = 0
+        self.seconds = 0
     }
 }
 
