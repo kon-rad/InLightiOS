df1cdbb73829856a411a99f12e87cb27b496f1cd
Konrad Gnat <konradmgnat@gmail.com>
2022-03-05 14:22:34 -0800

    login

--- InLightMeditation/Views/LoginView.swift
+++ InLightMeditation/Views/LoginView.swift
@@ -13,6 +13,7 @@ struct LoginView: View {
     @State private var password: String = ""
     @State private var confirmPassword: String = ""
     @State private var isLogin: Bool = true
+    @State private var isMagicLogin: Bool = false
     
     @EnvironmentObject var session: FirebaseSession
     
@@ -35,13 +36,34 @@ struct LoginView: View {
                             print("commit")
                         }
                     )
+                    .textFieldStyle(.roundedBorder)
                     .padding(10)
+                    
+                    SecureField(
+                        "Password",
+                        text: $password
+                    )
+                    .padding(10)
+                    .textFieldStyle(.roundedBorder)
+                    if !isLogin {
+                        SecureField(
+                            "Confirm Password",
+                            text: $confirmPassword
+                        )
+                        .padding(10)
+                        .textFieldStyle(.roundedBorder)
+                        
+                    }
                     Button(action: { loginOrSignup() }) {
                         AuthButtonContent(isLogin: $isLogin)
                     }
-                    .padding(10)
+                    .padding(40)
                     Button(action: { toggleIsLogin() }) {
                         Text("Login")
+                    }
+                    .padding(10)
+                    Button(action: { toggleIsLogin() }) {
+                        Text("Sign In With Email Link")
                     }
                     .padding(10)
                         
@@ -67,62 +89,81 @@ struct LoginView: View {
     func showMessagePrompt(message: String) {
         print("message: ", message)
     }
+    
     func loginOrSignup() {
-        let actionCodeSettings = ActionCodeSettings()
         
-        let scheme = "https"
-        let uriPrefix = "inlight.page.link"
-        let queryItemEmailName = "email"
-        
-        var components = URLComponents()
-        components.scheme = scheme
-        components.host = uriPrefix
-        
-        
-        actionCodeSettings.url = URL(string: "https://inlight-281fb.firebaseapp.com")
-        // The sign-in operation has to always be completed in the app.
-        actionCodeSettings.handleCodeInApp = true
-        actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
-        
-        Auth.auth().sendSignInLink(toEmail: email,
-                                   actionCodeSettings: actionCodeSettings) { error in
-          // ...
-            if let error = error {
-                self.showMessagePrompt(message: error.localizedDescription)
-              return
+        if isMagicLogin {
+            
+            // MARK - sign in with email link
+            
+            let actionCodeSettings = ActionCodeSettings()
+            
+            let scheme = "https"
+            let uriPrefix = "inlight.page.link"
+            let queryItemEmailName = "email"
+            
+            var components = URLComponents()
+            components.scheme = scheme
+            components.host = uriPrefix
+            components.path = "/open"
+            
+            let emailURLQueryItem = URLQueryItem(name: queryItemEmailName, value: email)
+            
+            guard let linkParameters = components.url else { return }
+            print("link param is: ", linkParameters);
+            
+            actionCodeSettings.url = linkParameters
+            
+            components.queryItems = [emailURLQueryItem]
+            
+            actionCodeSettings.url = linkParameters
+            // The sign-in operation has to always be completed in the app.
+            actionCodeSettings.handleCodeInApp = true
+            actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
+            
+            Auth.auth().sendSignInLink(
+                toEmail: email,
+                actionCodeSettings: actionCodeSettings
+            ) { error in
+              // ...
+                if let error = error {
+                    self.showMessagePrompt(message: error.localizedDescription)
+                  return
+                }
+                // The link was successfully sent. Inform the user.
+                // Save the email locally so you don't need to ask the user for it again
+                // if they open the link on the same device.
+                UserDefaults.standard.set(email, forKey: "Email")
+                self.showMessagePrompt(message: "Check your email for link")
+                // ...
             }
-            // The link was successfully sent. Inform the user.
-            // Save the email locally so you don't need to ask the user for it again
-            // if they open the link on the same device.
-            UserDefaults.standard.set(email, forKey: "Email")
-            self.showMessagePrompt(message: "Check your email for link")
-            // ...
+            return;
         }
         
-        
-//        if isLogin {
-//            session.logIn(email: email, password: password) { (result, error) in
-//                if error != nil {
-//                    print("Login Error", error)
-//                } else {
-//                    self.email = ""
-//                    self.password = ""
-//                    print("logged in")
-//                }
-//            }
-//        } else {
-//            if !email.isEmpty && !password.isEmpty {
-//                session.signUp(email: email, password: password) { (result, error) in
-//                    if error != nil {
-//                        print("Signup Error", error)
-//                    } else {
-//                        self.email = ""
-//                        self.password = ""
-//                        print("signed up")
-//                    }
-//                }
-//            }
-//        }
+        // MARK login or sign up with password
+        if isLogin {
+            session.logIn(email: email, password: password) { (result, error) in
+                if error != nil {
+                    print("Login Error", error)
+                } else {
+                    self.email = ""
+                    self.password = ""
+                    print("logged in")
+                }
+            }
+        } else {
+            if !email.isEmpty && !password.isEmpty {
+                session.signUp(email: email, password: password) { (result, error) in
+                    if error != nil {
+                        print("Signup Error", error)
+                    } else {
+                        self.email = ""
+                        self.password = ""
+                        print("signed up")
+                    }
+                }
+            }
+        }
     }
 }
 
